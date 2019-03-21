620a846b33d63f1c2743a4e3ccc20bc832922090
Yi Feng <yfxie@me.com>
2019-03-21 18:35:32 +0800

    feat. command deploy - develop deploy scripts - read host environment variables

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,9 @@ const path = require('path');
 const execSync = require('child_process').execSync;
 const JEKPACK_PATH = path.join(__dirname, '..');
 const APP_PATH = process.cwd();
+const s3EasyDeploy = require('s3-easy-deploy');
+
+require('dotenv').config({ path: path.resolve(APP_PATH, '.env') });
 
 program
   .command('dev')
@@ -38,4 +41,25 @@ program
       stdio: 'inherit'
     });
   });
+
+program
+  .command('deploy <bucket>')
+  .description('deploy the distribution to s3 bucket')
+  .option('-p, --private', 'upload files with private ACL')
+  .option('--cloud-front-id <cloudFrontId>', 'The CloudFront distribution id')
+  .action((bucket, options)=> {
+    const config = {
+      bucket,
+      concurrentRequests: 30,
+      publicRoot: path.join(APP_PATH, 'dist'),
+    };
+    if (options.acl) {
+      config.acl = 'private';
+    }
+    if (options.cloudFrontId) {
+      config.cloudFrontId = options.cloudFrontId;
+    }
+    s3EasyDeploy.deploy(config);
+  });
+
 program.parse(process.argv);
