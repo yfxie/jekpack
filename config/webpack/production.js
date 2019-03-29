1128eca0d470f3f6f085f8880299444f2fb96de1
Yi Feng <yfxie@me.com>
2019-03-30 00:05:47 +0800

    tweak

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const getConfigFilePath = require('../../lib/utils/getConfigFilePath');
+const getConfigFilePath = require(path.resolve(process.env.JEKPACK_ROOT, 'lib/utils/getConfigFilePath'));
 const baseConfig = require(getConfigFilePath('config/webpack/base.js'));
 const merge = require('webpack-merge');
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
